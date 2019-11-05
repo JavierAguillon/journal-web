c9af8f8cd7a347427c103da7b5b03232228b3fe0
Javier Aguillon <00078813@uca.edu.sv>
2019-11-05 00:00:35 -0600

    Updates

---
+++ precache-manifest.56c00e8a01b6a8f99c0325fc078a2691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123b803ee19a127b66d68755f19ae488",
+    "url": "/journal-web/index.html"
+  },
+  {
+    "revision": "b72bcb8da870d640580a",
+    "url": "/journal-web/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b72bcb8da870d640580a",
+    "url": "/journal-web/static/js/2.68408dae.chunk.js"
+  },
+  {
+    "revision": "73ee9c7575a32dac7e9b",
+    "url": "/journal-web/static/js/main.1b1ec1fd.chunk.js"
+  },
+  {
+    "revision": "291f5bea3902ebc9aa03",
+    "url": "/journal-web/static/js/runtime-main.314ba360.js"
+  }
+]);
